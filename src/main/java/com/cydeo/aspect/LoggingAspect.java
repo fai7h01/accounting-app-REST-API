a1536e42db1570fdc11e67cc8740fb53646259ed
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-20 20:57:05 +0400

    logging aspect

--- src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,7 +1,8 @@
 package com.cydeo.aspect;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +17,27 @@ public class LoggingAspect {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         SimpleKeycloakAccount userDetails = (SimpleKeycloakAccount) authentication.getDetails();
         return userDetails.getKeycloakSecurityContext().getToken().getPreferredUsername();
+    }
+
+    @Pointcut("within(com.cydeo.controller.*)")
+    public void anyControllerOperation() {};
+
+    @Before("anyControllerOperation()")
+    public void beforeAnyControllerOperationAdvice(JoinPoint joinPoint){
+        log.info("Before -> Method: {}, User: {}",
+                joinPoint.getSignature().toShortString(), getUsername());
+    }
+
+    @AfterReturning(value = "anyControllerOperation()", returning = "result")
+    public void afterReturningAnyControllerOperationAdvice(JoinPoint joinPoint, Object result){
+        log.info("After Returning -> Method: {}, User: {}, Result: {}",
+                joinPoint.getSignature(), getUsername(), result.toString());
+    }
+
+    @AfterThrowing(value = "anyControllerOperation()", throwing = "exception")
+    public void afterThrowingAnyControllerExceptionAdvice(JoinPoint joinPoint, Exception exception){
+        log.info("After Throwing -> Method: {}, User: {}, Exception: {}",
+                joinPoint.getSignature(), getUsername(), exception.getMessage());
     }
 
 
