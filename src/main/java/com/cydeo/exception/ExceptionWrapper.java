b07aad59cec0d417388b859c97e472fb5d9dbabf
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-21 22:48:38 +0400

    exception handling

--- src/main/java/com/cydeo/exception/ExceptionWrapper.java
+++ src/main/java/com/cydeo/exception/ExceptionWrapper.java
@@ -1,7 +1,9 @@
 package com.cydeo.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.ValidationException;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionWrapper {
 
@@ -18,4 +21,10 @@ public class ExceptionWrapper {
     private String message;
     private String path;
 
+    public ExceptionWrapper(Integer status, String message, String path) {
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.timestamp = LocalDateTime.now();
+    }
 }
--- src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
@@ -1,7 +1,22 @@
 package com.cydeo.exception;
 
+import com.cydeo.dto.common.ResponseWrapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ExceptionWrapper> userNotFoundExceptionHandler(UserNotFoundException exception, HttpServletRequest request){
+        exception.printStackTrace();
+        String message = exception.getMessage();
+        ExceptionWrapper exceptionWrapper = new ExceptionWrapper(HttpStatus.NOT_FOUND.value(), message, request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionWrapper);
+    }
+
 }
