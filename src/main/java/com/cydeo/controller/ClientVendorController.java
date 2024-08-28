201fdc13614e24bb3fed272d06ac5b9e0b68dfff
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-28 22:06:28 +0400

    swagger

--- src/main/java/com/cydeo/controller/ClientVendorController.java
+++ src/main/java/com/cydeo/controller/ClientVendorController.java
@@ -3,6 +3,8 @@ package com.cydeo.controller;
 import com.cydeo.dto.ClientVendorDto;
 import com.cydeo.dto.common.response.ResponseWrapper;
 import com.cydeo.service.ClientVendorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/clientVendor")
 @RequiredArgsConstructor
+@Tag(description = "Client/Vendor Controller", name = "Client/Vendor API")
 public class ClientVendorController {
 
     private final ClientVendorService clientVendorService;
 
     @GetMapping("/list")
+    @Operation(summary = "List all client/vendors")
     public ResponseEntity<ResponseWrapper> listAllClientVendors(){
         List<ClientVendorDto> clientVendorDtoList = clientVendorService.listAllClientVendorsForLoggedInCompany();
         return ResponseEntity.ok(ResponseWrapper.builder().code(HttpStatus.OK.value())
@@ -27,6 +31,7 @@ public class ClientVendorController {
     }
 
     @PostMapping
+    @Operation(summary = "Create client/vendor")
     public ResponseEntity<ResponseWrapper> createClientVendor(@RequestBody ClientVendorDto clientVendor){
         ClientVendorDto clientVendorDto = clientVendorService.save(clientVendor);
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseWrapper.builder()
@@ -37,6 +42,7 @@ public class ClientVendorController {
     }
 
     @PutMapping("/update/{id}")
+    @Operation(summary = "Update client/vendor")
     public ResponseEntity<ResponseWrapper> updateClientVendor(@RequestBody ClientVendorDto clientVendor, @PathVariable Long id){
         ClientVendorDto updatedClientVendor = clientVendorService.update(id, clientVendor);
         return ResponseEntity.ok(ResponseWrapper.builder()
@@ -47,6 +53,7 @@ public class ClientVendorController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete client/vendor")
     public ResponseEntity<ResponseWrapper> deleteClientVendor(@PathVariable Long id){
         clientVendorService.delete(id);
         return ResponseEntity.ok(ResponseWrapper.builder()
